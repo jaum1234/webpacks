ae98b4854be3288a11324d0fd5a33479e1b13751
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-01-26 07:12:13 -0300

    yarn

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 
 //Recomendado nomear a constante com o nome do plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './app/src/js/app.js',
@@ -11,16 +12,24 @@ module.exports = {
         path: path.resolve(__dirname, 'app/dist'), // Garante que o caminho absoluto funcione em qualquer SO
         clean: true,
     },
+    module: {
+        rules:  [
+            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './app/src/app.html',
             filename: 'app.html',
             hash: true
         }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        })
         //new CopyWebpackPlugin({
         //    patterns: [
         //        { from: './app/src/css', to: 'css' }
         //    ]
         //})
-    ]
+    ],
 };
